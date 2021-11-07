4d810111efe7e15c5364cccd993e43ee5159360a
joaogabrielmp <joaogabrielma@gmail.com>
2021-11-07 16:58:44 -0300

    feat: setup cors

--- backend/src/main/config/middlewares.ts
+++ backend/src/main/config/middlewares.ts
@@ -1,6 +1,8 @@
+import { cors } from '@/main/config/cors'
 import { Express, json } from 'express'
 
 export const setupMiddlewares = (app: Express): void => {
+  app.use(cors)
   app.use(json())
   app.use((req, res, next) => {
     res.type('json')
