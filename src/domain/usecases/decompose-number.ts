35b6c7d92ade69e843dac9c17d9a05b6d86dade3
joaogabrielmp <joaogabrielma@gmail.com>
2021-11-07 13:16:23 -0300

    feat: add divisor numbers logic

--- src/domain/usecases/decompose-number.ts
+++ src/domain/usecases/decompose-number.ts
@@ -2,9 +2,17 @@ import { DecomposeNumber as Interface } from '@/domain/interfaces'
 
 export class DecomposeNumber implements Interface {
   async execute ({ number }: Interface.Input): Promise<Interface.Output> {
+    const divisorNumbers = []
+
+    for (let index = 0; index <= number; index++) {
+      if (number % index === 0) {
+        divisorNumbers.push(index)
+      }
+    }
+
     const result = {
       entryNumber: number,
-      divisorNumbers: [],
+      divisorNumbers,
       primeDividers: []
     }
 
