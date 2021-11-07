85d260f6f73ccdb4fec9f0e072ddaaecd359f66f
joaogabrielmp <joaogabrielma@gmail.com>
2021-11-07 16:07:26 -0300

    test: is number validation cover

---
+++ tests/application/validation/is-number.spec.ts
@@ -0,0 +1,26 @@
+import { InvalidParamError } from '@/application/errors'
+import { IsNumber } from '@/application/validation'
+
+const field = 'any_field'
+
+type SutTypes = {
+  sut: IsNumber
+}
+
+const makeSut = (): SutTypes => {
+  const sut = new IsNumber(field)
+  return { sut }
+}
+
+describe('IsNumberValidation', () => {
+  test('Should return an InvalidParamError if validation fails', () => {
+    const { sut } = makeSut()
+    const error = sut.validate({ [field]: '1' })
+    expect(error).toEqual(new InvalidParamError(field))
+  })
+
+  test('Should not return if validation succeeds', () => {
+    const { sut } = makeSut()
+    expect(sut.validate({ [field]: 1 })).toBe(undefined)
+  })
+})
